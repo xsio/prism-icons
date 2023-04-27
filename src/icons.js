d8fd422ecf9169ebc61a8a1ab71ce25682dcdf15
wanlunhe <105707312+wanlunhe@users.noreply.github.com>
2023-04-27 17:23:49 +0800

    Update icons.js

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,3 @@
-export { default as ArrowDownRight } from './icons/arrow-down-right';
 export { default as ArrowLeft } from './icons/arrow-left';
 export { default as ArrowLeftCircle } from './icons/arrow-left-circle';
 export { default as ArrowRight } from './icons/arrow-right';
